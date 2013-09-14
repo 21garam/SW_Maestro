71901c1130653eeb6c5f0e0f0ee343ee03df6313
Yi hangaram <atria2005@naver.com>
2013-09-14 13:27:12 +0900

    1단계 1차 프로젝트 업로드
    기획 단계의 가설 검증에 사용될 1단계 1차 프로젝트 업로드.

---
+++ 물고기 키우기(1차 결과물)/Assets/Script/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	bool isLoad = false;
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+		#if UNITY_EDITOR
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel("GamePlayScene");
+		}
+		#elif UNITY_ANDROID
+		if(isLoad)
+			return;
+		foreach(Touch touch in Input.touches){
+			if(touch.phase == TouchPhase.Began){
+				Application.LoadLevel("GamePlayScene");
+				isLoad = true;
+				break;
+			}
+		}
+		
+		#endif
+	}
+}
