5473f7ac18b739cc954ee790e02f68a4389a3d87
Yi hangaram <atria2005@naver.com>
2013-09-16 19:17:10 +0900

    러너형 물고기 키우기 데모 업로드
    - 핵심 재미요소 테스트용 러너형 물고기 키우기 데모 업로드

---
+++ Preproduction/러너형 물고기 키우기 데모_이한가람/Assets/Script/BackGround.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGround : MonoBehaviour {
+	
+	float deltaTime = 0;
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+		tk2dSprite sprite = GetComponent<tk2dSprite>();
+		deltaTime -= Time.deltaTime;
+		sprite.transform.position = new Vector3(deltaTime*2, Player.Instance.playerPosition.y/10+4, 20);
+	}
+}
