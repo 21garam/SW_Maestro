66ef845db86a22cb953e2b321d65c446d0715967
Ninja86 <kms860226@naver.com>
2013-10-03 14:09:29 +0900

    MenuGame Touch Input at Android

--- Preproduction/runner - yi/Assets/Script/MainMenu.cs
+++ Preproduction/runner - yi/Assets/Script/MainMenu.cs
@@ -8,9 +8,14 @@ public class MainMenu : MonoBehaviour {
 	}
 	
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
+		#if UNITY_EDITOR
+		if(Input.GetKeyDown(KeyCode.Space)){
 			Application.LoadLevel("GamePlayScene");
 		}
+		#elif UNITY_ANDROID
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended){
+			Application.LoadLevel("GamePlayScene");
+		}
+		#endif
 	}
 }
