9200a68501a8e5b427f99c656ae6de0f87aadb61
Ninja86 <kms860226@naver.com>
2013-10-08 21:16:19 +0900

    [김민수] Unity Server Test UI 폴더 및 프로젝트 추
    로그인 부분 까지 구현.

---
+++ Preproduction/Sever - KMS/TestUI/Assets/Script/Login/Login_.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login_ : MonoBehaviour {
+	public tk2dUITextInput login_email;
+	public tk2dUITextInput login_password;
+	
+	public tk2dUIItem loginBtr;
+	public tk2dUIItem createBtr;
+	public tk2dUIItem backBtr;
+	public tk2dUIItem confirmBtr;
+	public Camera cam;
+	
+	WWW_ www;
+	
+	void Start () {
+		www = GetComponent<WWW_>();
+	} 
+	
+    void OnEnable() {
+        loginBtr.OnClick += Login;
+		confirmBtr.OnClick += Confirm;
+		createBtr.OnClick += GoToCreateAccount;
+		backBtr.OnClick += GoToBack;
+	}
+	
+    private void GoToCreateAccount() {
+        GoToLocation(cam, new Vector3(3, 1, -10));
+	}
+	
+	private void GoToBack() {
+		GoToLocation(cam, new Vector3(0, 1, -10));
+	}
+	
+	private void Confirm() {
+		www.CreateAccount(login_email.Text, login_password.Text);
+	}
+	
+	private void Login() {
+		www.Login(login_email.Text, login_password.Text, LoadLevel);
+	}
+	
+	public void LoadLevel(string str){
+		if(str == "ok")
+			Application.LoadLevel("Menu");
+	}
+	
+	private void GoToLocation(Camera obj, Vector3 loc) {
+		Transform t = obj.transform;
+    	StartCoroutine(TransformTo(t, 0.3f, loc));
+    }
+	
+	IEnumerator TransformTo( Transform transform, float time, Vector3 toPos) {
+        Vector3 fromPos = transform.localPosition;
+        for (float t = 0; t < time; t += tk2dUITime.deltaTime) {
+            float nt = Mathf.Clamp01( t / time );
+            nt = Mathf.Sin(nt * Mathf.PI * 0.5f);
+
+            transform.localPosition = Vector3.Lerp( fromPos, toPos, nt );
+            yield return 0;
+        }
+		transform.localPosition = toPos;
+    }
+}
---
+++ Preproduction/Sever - KMS/TestUI/Assets/Script/Login/WWW_.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WWW_ : MonoBehaviour {
+	public delegate void CallBackPtr(string str);
+	private string URL = "http://localhost:11080/";
+	
+	public void Login(string id, string password, CallBackPtr func){
+		WWWForm form = new WWWForm();
+		form.AddField("action", "Login");
+		form.AddField("id",id);
+		form.AddField("password",password);
+		StartCoroutine(WaitingForResponse(new WWW(URL, form), func));
+	}
+	
+	public void CreateAccount(string id, string password){
+		WWWForm form = new WWWForm();
+		form.AddField("action", "Create");
+		form.AddField("id",id);
+		form.AddField("password",password);
+		StartCoroutine(WaitingForResponse(new WWW(URL, form), null));
+	}
+	
+	/*
+	public void SendScore(int score, string name){
+		WWWForm form = new WWWForm();
+		form.AddField("","");
+		form.AddField("action", "Login");
+		form.AddField("score", score);
+		form.AddField("name", name);
+		StartCoroutine(WaitingForResponse(new WWW(URL, form), null));
+	}
+	*/
+	
+	public IEnumerator WaitingForResponse(WWW www, CallBackPtr callBackPtr){
+		yield return www;
+		if(www.error == null){
+			Debug.Log("WWW Link is Successful.");
+			Debug.Log(www.text);
+		}
+		else
+			Debug.Log("WWW Link is Failed.");
+		
+		
+		if(callBackPtr != null){
+			callBackPtr(www.text);
+			callBackPtr = null;
+		}
+		
+		www.Dispose();
+	}
+	
+	/*
+	public WWW GET(string url) 
+	{ 
+		WWW www = new WWW(url); 
+		StartCoroutine(WaitForRequest(www)); 
+		return www; 
+	}
+	
+	public WWW POST(string url, Dictionary<string, string> post) 
+	{ 
+		WWWForm form = new WWWForm(); 
+ 		foreach (KeyValuePair<string, string> post_arg in post){ 
+			form.AddField(post_arg.Key, post_arg.Value); 
+		} 
+		WWW www = new WWW(url, form); 
+		StartCoroutine(WaitForRequest(www)); 
+		return www;
+	} 
+	
+	private IEnumerator WaitForRequest(WWW www) {
+		yield return www;
+		if (www.error == null){ 
+			Debug.Log("WWW Ok!: " + www.text);
+		} 
+		else{ 
+			Debug.Log("WWW Error: " + www.error);
+		}
+	} 
+	*/
+}
