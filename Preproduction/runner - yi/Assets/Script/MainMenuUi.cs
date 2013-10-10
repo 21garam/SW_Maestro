16022f7b686e9094f5235370a10ca0f1e34696b6
21garam <atria2005@naver.com>
2013-10-10 18:31:26 +0900

    User Flow 텍스트 아틀라스 교체
    텍스트 아틀라스 관련 오류가 발생하여 텍스트 아틀라스 교체

---
+++ Preproduction/runner - yi/Assets/Script/MainMenuUi.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUi : MonoBehaviour {
+	public tk2dUIItem playGameButton;
+	
+	void OnEnable()
+	{
+		playGameButton.OnClick += ClickButton;
+	}
+	
+	void OnDisable()
+	{
+		playGameButton.OnClick -= ClickButton;
+	}
+	
+	void ClickButton()
+	{
+		Application.LoadLevel("Ready_Scene");
+	}
+}
