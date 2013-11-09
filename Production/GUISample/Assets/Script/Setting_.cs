0247e43bdebcd2f6c688d25980e7a3159aaa8155
Ninja86 <kms860226@naver.com>
2013-11-10 01:12:31 +0900

    [김민수] 성호 Commit Test

---
+++ Production/GUISample/Assets/Script/Setting_.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Setting_ {
+	public static string settingFileName = "setting.txt";
+	public static string playerID;
+}
---
+++ Production/GUISample/Assets/Script/Title_.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title_ : MonoBehaviour {
+	public Camera cam;
+	
+	void Start () {
+		//Debug.Log(cam.pixelRect);
+		//Debug.Log(cam.rect);	
+	}
+	
+	void Update () {
+	
+	}
+}
