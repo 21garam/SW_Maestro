af28a800aa5a0d65fed0c5d73d1b654c0418bf21
chosungho <erty7331@naver.com>
2013-11-16 11:35:09 +0900

    [조성호] main Scene 랭킹구
    prefab을 이용하여 깔끔하게 보이도록

--- Production/RealGame/Assets/WorkFlow/Scripts/Server/Sample/RankingListCode_.cs
+++ Production/RealGame/Assets/WorkFlow/Scripts/Server/Sample/RankingListCode_.cs
@@ -14,9 +14,14 @@ public class RankingListCode_ : MonoBehaviour {
 	
 	public tk2dTextMesh textPrefabs;
 	
-	const int listCount = 10;		
+	public GameObject listPrefabs;
 	
+	const int listCount = 10;
+	
+	List<GameObject> gamelist = new List<GameObject>(listCount);
 	List<tk2dTextMesh> textList = new List<tk2dTextMesh>(listCount);
+	
+	
 	
 	void Start(){
 		GetRankingList();
@@ -33,21 +38,46 @@ public class RankingListCode_ : MonoBehaviour {
 	
 	private void XMLParseToList(string xml){
 		if(XMLParser_.RankingInfoXMLParse(xml)){
-			/*tk2dTextMesh fistTextElement = GameObject.Instantiate(textPrefabs) as tk2dTextMesh;
+			int listCount = XMLParser_.OtherUserRankingInfoList.Count;
+			
+			/*
+			tk2dTextMesh fistTextElement = GameObject.Instantiate(textPrefabs) as tk2dTextMesh;
 			fistTextElement.transform.parent = list.contentContainer.transform;
 			fistTextElement.transform.localPosition = new Vector3(0.05f, - 0.02f, 0);
 			fistTextElement.text = XMLParser_.PlayerUserRankingInfo.ToString();
 			fistTextElement.Commit();	
-			textList.Add(fistTextElement);*/
+			textList.Add(fistTextElement);
+			*/
 			
-			int listCount = XMLParser_.OtherUserRankingInfoList.Count;
+			/*
 			for(int i = 0; i < listCount; i++){
 				tk2dTextMesh textElement = GameObject.Instantiate(textPrefabs) as tk2dTextMesh;
 				textElement.transform.parent = list.contentContainer.transform;
-				textElement.transform.localPosition = new Vector3(0.35f, -(i * 0.35f) - 0.15f, 0);
+				textElement.transform.localPosition = new Vector3(0.3f, -(i * 0.35f) - 0.15f, 0);
 				textElement.text = XMLParser_.OtherUserRankingInfoList[i].ToString();
 				textElement.Commit();
 				textList.Add(textElement);
+			}
+			*/
+			
+			
+			for(int i = 0;i< listCount; i++)
+			{
+				GameObject listElement = GameObject.Instantiate(listPrefabs) as GameObject;
+				listElement.transform.parent = list.contentContainer.transform;
+				listElement.transform.localPosition = new Vector3(-0.14f, -(i * 0.35f)-0.1f, 0);
+				listElement.transform.localScale = new Vector3(1f, 1f, 1f);
+				
+				listElement.transform.FindChild("Text_rank").localPosition = new Vector3(0.5f, -0.03f, 0);
+				listElement.transform.FindChild("Text_rank").GetComponent<tk2dTextMesh>().text = XMLParser_.OtherUserRankingInfoList[i].ranking.ToString();
+				listElement.transform.FindChild("Text_rank").GetComponent<tk2dTextMesh>().Commit();
+				
+				listElement.transform.FindChild("Text_score").localPosition = new Vector3(1.94f, -0.03f, 0);
+				listElement.transform.FindChild("Text_score").GetComponent<tk2dTextMesh>().text = XMLParser_.OtherUserRankingInfoList[i].score.ToString();
+				listElement.transform.FindChild("Text_score").GetComponent<tk2dTextMesh>().Commit();
+				
+				listElement.Commit();
+				gamelist.Add(listElement);
 			}
 		}
 		else{
