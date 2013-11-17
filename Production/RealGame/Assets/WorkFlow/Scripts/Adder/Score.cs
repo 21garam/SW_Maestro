482c9aab6a144f79c8fc7009fcaa28e6c7c53fc1
chosungho <erty7331@naver.com>
2013-11-17 14:43:57 +0900

    [조성호] MsgBox 수

--- Production/RealGame/Assets/WorkFlow/Scripts/Adder/Score.cs
+++ Production/RealGame/Assets/WorkFlow/Scripts/Adder/Score.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class Score : MonoBehaviour {
 	public tk2dTextMesh score;
 	
+	public WWW_ www;
+	
 	public int score_int;
 	// Use this for initialization
 	void Start () {
 		score_int = SharedData.Instance.score;
 		score.text = score_int.ToString();
+		if(GUI_Setting_.PLAYER_ID != ""){
+			//Debug.Log("ID : " + GUI_Setting_.PLAYER_ID);
+			www.UpdateAccount(GUI_Setting_.PLAYER_ID, WWW_.INTEGER_NULL, score_int);
+		}
 	}
 }
